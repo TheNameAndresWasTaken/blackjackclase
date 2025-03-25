c431e4b78d8d255ea84c69ea3b2e106f8500cc67
Andres David <andresdavidagudelo01@gmail.com>
2025-03-25 10:59:18 -0500

    Agregadas 2 dataclasses con sus funcionalidades

---
+++ blackjack/model.py
@@ -0,0 +1,42 @@
+import random
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import ClassVar
+
+CORAZON = "\u2764\uFE0F"
+TREBOL = "\u2663\uFE0F"
+DIAMANTE = "\u2666\uFE0F"
+ESPADA = "\u2660\uFE0F"
+OCULTA = "\u25AE\uFE0F"
+
+
+@dataclass
+class Carta:
+    VALORES: ClassVar[list[str]] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"]
+    PINTAS: ClassVar[list[str]] = [CORAZON, TREBOL, DIAMANTE, ESPADA]
+    pinta: str
+    valor: str
+    oculta: bool = True
+
+@dataclass
+class Baraja:
+    cartas: list[Carta] = field(init=False, default_factory=list)
+
+    def reiniciar(self):
+        self.cartas.clear()
+        for pinta in Carta.PINTAS:
+            for valor in Carta.VALORES:
+                self.cartas.append(Carta(pinta, valor))
+
+    def revolver(self):
+        random.shuffle(self.cartas)
+
+    def repartir_cartas(self, oculta: bool = False) -> Carta | None:
+        if len(self.cartas) > 0:
+            carta = self.cartas.pop()
+            if oculta:
+                carta.ocultar()
+
+            return carta
+        else:
+            return None
